e7cacca968d7db02835acc32432801814708c2d3
coleb <cole.ballard97@gmail.com>
2023-08-09 17:36:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const { promisify } = require('util')
 const readFile = promisify(fs.readFile)
 const GPT_MODE = process.env.GPT_MODE
 
-let file_context = "You are a helpful Twitch Chatbot."
+let file_context = "You are a twitch streamer named pyyros or Drew."
 
 const messages = [
-  {role: "system", content: "You are a helpful Twitch Chatbot."}
+  {role: "system", content: file_context}
 ];
 
 console.log("GPT_MODE is " + GPT_MODE)
